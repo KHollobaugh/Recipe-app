006c16e44d23415d5067ef8f8f5b725e4a9a81c8
KHollobaugh <khollobaugh@gmail.com>
2019-01-02 21:23:09 -0500

    recipe database

---
+++ data/migrations/20190102210749_recipes.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('recipes', function(tbl){
+      tbl.increments();
+      tbl.string('name', 250).notNullable();
+      tbl.string('link', 100).notNullable();
+      tbl.string('tag', 50);
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('recipes')
+};
